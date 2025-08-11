87ed2d1110138e7f81d0aa06e09707ef0d6a0c43
Your Name <you@example.com>
2025-08-11 22:47:28 +0700

    fix

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,10 @@
-import yoruka from "./assets/yoruka.jpg"
+import yori from "./assets/yori.jpg"
 
 function Card(){
     return(
         <div className="card">
-            <img className="gambar" src={yoruka} alt="Asanagi yori" />
-            <h2 className="card-title">Asanagi Yori</h2>
+            <img className="gambar" src={yori} alt="Asanagi yori" />
+            <h2 className="card-title">ini adalah header card</h2>
             <p className="Card-text">ini adalah paragraf di dalam card</p>
         </div>
     );
